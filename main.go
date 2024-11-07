9b03ebea91f07776ea1f87e0597552dc16619263
tamjidahmed <mail.to.tamjid@gmail.com>
2024-11-07 15:32:06 +0600

    WIP monotonic increasing counter

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"log"
 )
 
-const key = "counter"
-
 type AddMsg struct {
 	Type  string `json:"type"`
 	Delta int    `json:"delta"`
@@ -17,15 +15,41 @@ type AddMsg struct {
 func main() {
 	n := maelstrom.NewNode()
 	kv := maelstrom.NewSeqKV(n)
+	key := n.ID()
+
+	n.Handle("latest", func(msg maelstrom.Message) error {
+		val, err := kv.ReadInt(context.Background(), key)
+		if err != nil && maelstrom.ErrorCode(err) != maelstrom.KeyDoesNotExist {
+			val = 0
+		}
+
+		return n.Reply(msg, map[string]int{"value": val})
+	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		val, err := kv.ReadInt(context.Background(), key)
-		if err != nil {
+		maxVal, err := kv.ReadInt(context.Background(), key)
+		if err != nil && maelstrom.ErrorCode(err) != maelstrom.KeyDoesNotExist {
+			panic(err)
 			return err
 		}
+		for _, k := range n.NodeIDs() {
+			if k == n.ID() {
+				continue
+			}
+			resp, err := n.SyncRPC(context.Background(), k, map[string]string{"type": "latest"})
+			if err != nil {
+				return err
+			}
+			body := make(map[string]int)
+			if err := json.Unmarshal(resp.Body, &body); err != nil {
+				return err
+			}
+			maxVal += body["value"]
+		}
+
 		return n.Reply(msg, map[string]any{
-			"type":     "read_ok",
-			"messages": val,
+			"type":  "read_ok",
+			"value": maxVal,
 		})
 	})
 
